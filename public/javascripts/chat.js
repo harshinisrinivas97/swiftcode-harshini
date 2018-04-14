9a562c90e7d424033275be01b8bee818ff3cf53b
harshinisrinivas97 <harshusrinivas97@gmail.com>
2018-04-14 12:12:59 +0000

    View phase-2

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,22 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
-
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+		},
+        {
+            'sender': 'BOT',
+            'text': 'Hi!!What can I do for you'
+		},
+        {
+            'sender': 'USER',
+            'text': 'Can you play some music!!'
+		},
+        {
+            'sender': 'BOT',
+            'text': 'Ok!!!Playing...'
+		}
+	];
 });
